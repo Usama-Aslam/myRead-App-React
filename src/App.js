0e1def2a48d5e89240b6ce9fb073c28fcb5f1fdf
UsamaAslam <uaslam345@gmail.com>
2020-05-11 20:16:33 +0500

    updated README

--- src/App.js
+++ src/App.js
@@ -114,8 +114,8 @@ class BooksApp extends React.Component {
     }
 
     BooksAPI.update(book, newShelf)
-      .then((data) => {
-        console.log("Book shelf changed and updated at server", data);
+      .then(() => {
+        console.log("Book shelf changed and updated at server");
       })
       .catch((err) => {
         console.log("Error updating shelf", err);
